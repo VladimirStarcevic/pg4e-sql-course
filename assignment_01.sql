1ed018d116302f2ef1a791b34568a6acf4da8cab
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-10-15 15:28:26 +0200

    Complete Assignment 01: Initial table setup

---
+++ assignment_01.sql
@@ -0,0 +1,22 @@
+CREATE TABLE pg4e_debug (
+                            id SERIAL,
+                            query VARCHAR(4096),
+                            result VARCHAR(4096),
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            PRIMARY KEY(id)
+);
+
+SELECT query, result, created_at FROM pg4e_debug;
+
+CREATE TABLE pg4e_result (
+                             id SERIAL,
+                             link_id INTEGER UNIQUE,
+                             score FLOAT,
+                             title VARCHAR(4096),
+                             note VARCHAR(4096),
+                             debug_log VARCHAR(8192),
+                             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                             updated_at TIMESTAMP
+);
+
+INSERT INTO pg4e_debug (query) VALUES ('Hello world');
